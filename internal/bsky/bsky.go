e3934a69785d65c6c78157cf1fb8e92bf22bcf97
Lucas DEBOUTÉ (codeinu) <lucas.deboute@gmail.com>
2024-11-25 22:51:58 +0100

    move out bsky code into internal pkg

---
+++ internal/bsky/bsky.go
@@ -0,0 +1,88 @@
+package bsky
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log/slog"
+
+	"github.com/bluesky-social/indigo/api/bsky"
+	"github.com/gorilla/websocket"
+)
+
+type Message struct {
+	DID    string  `json:"did"`
+	Commit *Commit `json:"commit,omitempty"`
+}
+
+type Commit struct {
+	Record json.RawMessage `json:"record,omitempty`
+}
+
+type CallbackOnMessageFunc func(context.Context, string) error
+
+// BSkyService is the BSky listener instance
+type BSkyService struct {
+	Account  string
+	Callback CallbackOnMessageFunc
+}
+
+func NewBSkyService() *BSkyService {
+	return &BSkyService{}
+}
+
+func (bs *BSkyService) WithListenedAccountDID(userdid string) *BSkyService {
+	bs.Account = userdid
+
+	return bs
+}
+
+func (bs *BSkyService) WithCallbackOnMessage(callback CallbackOnMessageFunc) *BSkyService {
+	bs.Callback = callback
+
+	return bs
+}
+
+func (bs *BSkyService) Listen(ctx context.Context) error {
+	d := websocket.DefaultDialer
+	msgchan := make(chan Message)
+
+	con, _, err := d.DialContext(ctx, fmt.Sprintf("wss://jetstream2.us-east.bsky.network/subscribe?wantedCollections=app.bsky.feed.post&wantedDids=%s", bs.Account), nil)
+	if err != nil {
+		return fmt.Errorf("dial failure: %w", err)
+	}
+
+	go func() {
+		for {
+			msg := Message{}
+			err := con.ReadJSON(&msg)
+			if err != nil {
+				slog.Error("connexion closed")
+				close(msgchan)
+				return
+			}
+
+			msgchan <- msg
+		}
+	}()
+
+	for {
+		message := Message{}
+
+		select {
+		case <-ctx.Done():
+			slog.Info("context canceled")
+			con.Close()
+			return nil
+		case message = <-msgchan:
+		}
+
+		var post bsky.FeedPost
+		if err = json.Unmarshal(message.Commit.Record, &post); err != nil {
+			slog.Error(err.Error())
+			continue
+		}
+
+		bs.Callback(ctx, post.Text)
+	}
+}
--- main.go
+++ main.go
@@ -2,83 +2,22 @@ package main
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
 	"log/slog"
 	"os"
 	"os/signal"
 	"syscall"
 
-	"github.com/bluesky-social/indigo/api/bsky"
+	intrnbsky "github.com/codeinuit/bsky-go-federate/internal/bsky"
 	"github.com/codeinuit/bsky-go-federate/internal/federation"
 	"github.com/codeinuit/bsky-go-federate/internal/federation/mastodon"
-	"github.com/gorilla/websocket"
 	"github.com/joho/godotenv"
 )
-
-type Message struct {
-	DID    string  `json:"did"`
-	Commit *Commit `json:"commit,omitempty"`
-}
-
-type Commit struct {
-	Record json.RawMessage `json:"record,omitempty`
-}
-
-func bskyJetstreamWithCallback(ctx context.Context, callback func(context.Context, string) error) error {
-	d := websocket.DefaultDialer
-	datachansempai := make(chan Message)
-
-	userdid := os.Getenv("BSKY_USER_DID")
-
-	ctx, stop := signal.NotifyContext(ctx, syscall.SIGINT)
-	defer stop()
-	con, _, err := d.DialContext(ctx, fmt.Sprintf("wss://jetstream2.us-east.bsky.network/subscribe?wantedCollections=app.bsky.feed.post&wantedDids=%s", userdid), nil)
-	if err != nil {
-		return fmt.Errorf("dial failure: %w", err)
-	}
-
-	// running reader loop
-	go func() {
-		for {
-			msg := Message{}
-			err := con.ReadJSON(&msg)
-			if err != nil {
-				slog.Error("connexion closed")
-				close(datachansempai)
-				return
-			}
-
-			datachansempai <- msg
-		}
-	}()
-
-	for {
-		message := Message{}
-
-		select {
-		case <-ctx.Done():
-			slog.Info("context canceled")
-			con.Close()
-			return nil
-		case message = <-datachansempai:
-		}
-
-		var post bsky.FeedPost
-		if err = json.Unmarshal(message.Commit.Record, &post); err != nil {
-			slog.Error(err.Error())
-			continue
-		}
-
-		callback(ctx, post.Text)
-	}
-}
 
 func main() {
 	var tooter federation.Federation
 
 	if err := godotenv.Load(); err != nil {
-		slog.Error("error occurred: " + err.Error())
+		slog.Error("could not loa: " + err.Error())
 
 		os.Exit(1)
 	}
@@ -87,8 +26,16 @@ func main() {
 	cid := os.Getenv("MASTODON_APP_CLIENT_ID")
 	csecret := os.Getenv("MASTODON_APP_CLIENT_SECRET")
 	at := os.Getenv("MASTODON_APP_ACCESS_TOKEN")
+	userdid := os.Getenv("BSKY_USER_DID")
 
 	tooter = mastodon.NewClient(host, cid, csecret, at)
 
-	bskyJetstreamWithCallback(context.Background(), tooter.Post)
+	lstnr := intrnbsky.NewBSkyService().
+		WithListenedAccountDID(userdid).
+		WithCallbackOnMessage(tooter.Post)
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT)
+	defer stop()
+
+	lstnr.Listen(ctx)
 }
